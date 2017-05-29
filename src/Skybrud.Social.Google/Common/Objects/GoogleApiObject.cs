72d1d8d94a7ea18af3d7082b7f9480f2e7434bfd
Anders Bjerner <abjerner@skybrud.dk>
2017-05-29 21:50:49 +0200

    The "GoogleApiObject" class should inherit from "JsonObjectBase"

--- src/Skybrud.Social.Google/Common/Objects/GoogleApiObject.cs
+++ src/Skybrud.Social.Google/Common/Objects/GoogleApiObject.cs
@@ -1,32 +1,20 @@
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json;
 
 namespace Skybrud.Social.Google.Common.Objects {
     
     /// <summary>
     /// Class representing an object in the Google ecosystem.
     /// </summary>
-    public class GoogleApiObject {
+    public class GoogleApiObject : JsonObjectBase {
 
-        #region Properties
-
-        /// <summary>
-        /// Gets a reference to the underlying instance of <code>JObject</code>.
-        /// </summary>
-        [JsonIgnore]
-        public JObject JObject { get; private set; }
-
-        #endregion
-
-        #region Constructor
+        #region Constructors
 
         /// <summary>
-        /// Initializes instance from the specified <code>obj</code>.
+        /// Initializes a new instance from the specified <paramref name="obj"/>.
         /// </summary>
-        /// <param name="obj">The instance of <code>JObject</code> representing the object.</param>
-        protected GoogleApiObject(JObject obj) {
-            JObject = obj;
-        }
+        /// <param name="obj">The instance of <see cref="JObject"/> representing the object.</param>
+        protected GoogleApiObject(JObject obj) : base(obj) { }
 
         #endregion
 
