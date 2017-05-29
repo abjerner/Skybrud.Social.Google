41971828e88a2767d48b54ed57a4039cade39f34
Anders Bjerner <abjerner@skybrud.dk>
2017-05-29 23:40:22 +0200

    YouTube: Typo

--- src/Skybrud.Social.Google/YouTube/Objects/Videos/YouTubeVideoFailureReason.cs
+++ src/Skybrud.Social.Google/YouTube/Objects/Videos/YouTubeVideoFailureReason.cs
@@ -6,7 +6,7 @@
     public enum YouTubeVideoFailureReason {
         Codec,
         Conversion,
-        EmptyFIle,
+        EmptyFile,
         InvalidFile,
         TooSmall,
         UploadAborted
